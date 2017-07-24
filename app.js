216f724d51380d7364ecafea1a1d90a792af9d4e
heizen13 <julzz.depaz@gmail.com>
2017-07-24 09:28:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://u4hhkjllfpbxzpa:eXPrDXL1tQTysT5IA9Lm@bmikeyxae8urdmd-mongodb.services.clever-cloud.com:27017/bmikeyxae8urdmd');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 
 
@@ -530,15 +537,8 @@ else if(session.message.text.toLowerCase().contains('sad')){
   
 else if(session.message.text.toLowerCase().contains('sample add')){
       session.send('┬──┬ ノ( ゜-゜ノ)');
+    
   
-  
-  var mongoose = require('mongoose');
-mongoose.connect('mongodb://u4hhkjllfpbxzpa:eXPrDXL1tQTysT5IA9Lm@bmikeyxae8urdmd-mongodb.services.clever-cloud.com:27017/bmikeyxae8urdmd');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
 var kittySchema = mongoose.Schema({
     name: String
 });
