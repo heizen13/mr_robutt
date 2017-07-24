96453846622965e8480c2f24c7f4ff5dbaae9d65
heizen13 <julzz.depaz@gmail.com>
2017-07-24 08:54:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -210,43 +210,9 @@ else if(session.message.text.toLowerCase().contains('copy ')){
   
   
   
-   else if(session.message.text.toLowerCase().contains('nyan')){
-   session.send(' 😎 ');
-     
-     var kittySchema = mongoose.Schema({
-    name: String
-});
-     var Kitten = mongoose.model('Kitten', kittySchema);
-     var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-     
-     // NOTE: methods must be added to the schema before compiling it with mongoose.model()
-kittySchema.methods.speak = function () {
-  var greeting = this.name
-    ? "Meow name is " + this.name
-    : "I don't have a name";
-  console.log(greeting);
-}
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-     
-   var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"  
-     
-   fluffy.save(function (err, fluffy) {
-  if (err) return console.error(err);
-  fluffy.speak();
-})
-    Kitten.find(function (err, kittens) {
-  if (err) return console.error(err);
-  console.log(kittens);
-}) 
-     
-   Kitten.find({ name: /^fluff/ }, callback);  
-   }
+  
    else{
-     
-        session.send('error..');
+session.send('error..');
       }
 });
  
